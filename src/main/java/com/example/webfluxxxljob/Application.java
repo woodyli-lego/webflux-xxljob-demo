65b19073df690aeb328fb84437c0dad7088740d7
Woody Li <woody.li@lego.com>
2024-08-07 22:30:13 +0800

    rename

--- src/main/java/com/example/webfluxxxljob/Application.java
+++ src/main/java/com/example/webfluxxxljob/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebfluxxxljobApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebfluxxxljobApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
